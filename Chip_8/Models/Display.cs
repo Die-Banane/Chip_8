61c6a2e2a80a8e11f21e66401f3ed5931500493a
Jonas Grammann <jonasgrammann@gmail.com>
2025-08-25 20:23:59 +0200

    corrected the Width property of the Fisplay class.

--- Chip_8/Models/Display.cs
+++ Chip_8/Models/Display.cs
@@ -3,7 +3,7 @@
 internal static class Display
 {
     public static int Width { get; } = 64;
-    public static int Height { get; } = 23;
+    public static int Height { get; } = 32;
 
     public static byte[] Buffer { get; set; } = new byte[Width * Height];
 }
