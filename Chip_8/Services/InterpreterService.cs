5ef88aeee0bd5b7e362adfeb6f1fc743e2e7a004
Jonas Grammann <jonasgrammann@gmail.com>
2025-12-14 20:01:28 +0100

    added missing Layouts

--- Chip_8/Services/InterpreterService.cs
+++ Chip_8/Services/InterpreterService.cs
@@ -13,6 +13,7 @@ public class InterpreterService(Keyboard keyboardService)
 {
     private readonly Dictionary<Key, byte> defaultKeyMap = new()
     {
+        { Key.D0, 0x0 },
         { Key.D1, 0x1 },
         { Key.D2, 0x2 },
         { Key.D3, 0x3 },
@@ -27,7 +28,47 @@ public class InterpreterService(Keyboard keyboardService)
         { Key.C, 0xc },
         { Key.D, 0xd },
         { Key.E, 0xe },
-        { Key.F, 0xf },
+        { Key.F, 0xf }
+    };
+    
+    private readonly Dictionary<Key, byte> qwertzKeyMap = new()
+    {
+        { Key.X, 0x0 },
+        { Key.D1, 0x1 },
+        { Key.D2, 0x2 },
+        { Key.D3, 0x3 },
+        { Key.Q, 0x4 },
+        { Key.W, 0x5 },
+        { Key.E, 0x6 },
+        { Key.A, 0x7 },
+        { Key.S, 0x8 },
+        { Key.D, 0x9 },
+        { Key.Y, 0xa },
+        { Key.C, 0xb },
+        { Key.D4, 0xc },
+        { Key.R, 0xd },
+        { Key.F, 0xe },
+        { Key.V, 0xf }
+    };
+
+    private readonly Dictionary<Key, byte> qwertyKeyMap = new()
+    {
+        { Key.X, 0x0 },
+        { Key.D1, 0x1 },
+        { Key.D2, 0x2 },
+        { Key.D3, 0x3 },
+        { Key.Q, 0x4 },
+        { Key.W, 0x5 },
+        { Key.E, 0x6 },
+        { Key.A, 0x7 },
+        { Key.S, 0x8 },
+        { Key.D, 0x9 },
+        { Key.Z, 0xa },
+        { Key.C, 0xb },
+        { Key.D4, 0xc },
+        { Key.R, 0xd },
+        { Key.F, 0xe },
+        { Key.V, 0xf }
     };
     
     public IInterpreter? CurrentInstance { get; private set; }
@@ -40,8 +81,13 @@ public class InterpreterService(Keyboard keyboardService)
 
         switch (options.Layout)
         {
-            case KeyPadLayouts.DefaultLayout:
-                keyMap = defaultKeyMap;
+            case KeyPadLayouts.Qwertz:
+                keyMap = qwertzKeyMap;
+                keyboardService.KeyMap = keyMap;
+                break;
+
+            case KeyPadLayouts.Qwerty:
+                keyMap = qwertyKeyMap;
                 keyboardService.KeyMap = keyMap;
                 break;
             
